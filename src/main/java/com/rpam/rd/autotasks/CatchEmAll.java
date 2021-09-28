fda79e2e832ecfa376234515bcab6c5f4438e9b8
Vladimir Vasyukov <vladimir.vasyukov7@gmail.com>
2021-09-28 21:31:21 +0300

    Exceptions - Catch`em all

--- src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
+++ src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
@@ -1,17 +1,50 @@
 package com.rpam.rd.autotasks;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class CatchEmAll {
+    static Exception exception;
+    final static private String IO_EXCEPTION = "IOException";
+    final static private String FILE_NOT_FOUND_EXCEPTION = "FileNotFoundException";
+    final static private String ARITHMETIC_EXCEPTION = "ArithmeticException";
+    final static private String NUMBER_FORMAT_EXCEPTION = "NumberFormatException";
 
-    //You may set another exception in this field;
-    static Exception exception = new FileNotFoundException();
 
     public static void riskyMethod() throws Exception {
-        throw exception;
+        if (exception != null) {
+            throw exception;
+        }
     }
 
     public static void main(String[] args) throws Exception {
-        riskyMethod();
+        if (args.length >= 1) {
+            switch (args[0]) {
+                case IO_EXCEPTION:
+                    exception = new IOException();
+                    break;
+                case FILE_NOT_FOUND_EXCEPTION:
+                    exception = new FileNotFoundException();
+                    break;
+                case ARITHMETIC_EXCEPTION:
+                    exception = new ArithmeticException();
+                    break;
+                case NUMBER_FORMAT_EXCEPTION:
+                    exception = new NumberFormatException();
+                    break;
+                default:
+                    System.out.println("Use arguments IOException, FileNotFoundException," +
+                        "ArithmeticException, NumberFormatException");
+            }
+        }
+        try {
+            riskyMethod();
+        } catch (FileNotFoundException e) {
+            throw new IllegalArgumentException("Resource is missing", e);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Resource error", e);
+        } catch (ArithmeticException | NumberFormatException e) {
+            System.err.print(e);
+        }
     }
 }
